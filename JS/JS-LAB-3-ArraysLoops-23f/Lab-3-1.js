5a08666b4245ceadabac76fc41ecb5296cfda3c5
Yash Modi <98708635+yashmodi9998@users.noreply.github.com>
2023-10-04 15:43:05 -0400

    Delete JS directory

--- JS/JS-LAB-3-ArraysLoops-23f/Lab-3-1.js
+++
@@ -1,7 +0,0 @@
-//LAB 3 - ARRAYS & LOOPS - PART 1
-
-var fruits=[];
-//ARRAY OF FRUITS
-fruits = ["Apple","Banana","Pineapple","Avocado","Strawberry"];
-//OUTPUT ONE FRUIT IN POPUP BOX
-alert("Favourite fruit is "+fruits[4]);
